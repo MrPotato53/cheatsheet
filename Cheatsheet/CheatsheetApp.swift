ca3a3fedd06d0cabacfd9746294ef23f3a2f5e22
William He <willhe9000@gmail.com>
2026-07-02 21:02:02 -0700

    Initial Commit

---
+++ Cheatsheet/CheatsheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheatsheetApp.swift
+//  Cheatsheet
+//
+//  Created by William He on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct CheatsheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheatsheetTests/CheatsheetTests.swift
@@ -0,0 +1,17 @@
+//
+//  CheatsheetTests.swift
+//  CheatsheetTests
+//
+//  Created by William He on 7/2/26.
+//
+
+import Testing
+@testable import Cheatsheet
+
+struct CheatsheetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
